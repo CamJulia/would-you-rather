ecd6e04d897e76713297698c4fff1cb59d9a9a13
Julia Çam <barbarbetsy@googlemail.com>
2019-07-20 20:44:30 +0200

    feat: add login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react'
-import Player from './Player'
+// import Player from './Player'
+import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
 
 class Login extends Component {
+  setAuthUser = (e) => {
+    const name = e.currentTarget.getAttribute('name');
+    this.props.dispatch(setAuthedUser(name))
+  }
+
   render() {
+
+    const { usersIds, users } = this.props;
+    if (!usersIds.length) return null;
+
+
     return (
       <div>
         <h1>Would you rather be...</h1>
-        <button>
-          <Player />
-        </button>
+        {usersIds.map((player) => {
+
+          return <button className='player' key={users[player].id} name={users[player].id} onClick={this.setAuthUser}>
+            <img src={users[player].avatarURL} alt={`Avatar of ${users[player].name}`} />
+            <p>{users[player].name}</p>
+          </button >
+        })}
       </div>
     )
   }
 }
 
-export default Login
+function mapStateToProps({ questions, authedUser, users }) {
+  return {
+    usersIds: Object.keys(users),
+    authedUser,
+    users,
+    questions
+  }
+}
+
+export default connect(mapStateToProps)(Login)
