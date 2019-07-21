ed2315645dac5f78daa1e4a8fb71daeba5abbe6d
Julia Çam <barbarbetsy@googlemail.com>
2019-07-21 12:20:38 +0200

    feat: add tiny stats

--- src/components/History.js
+++ src/components/History.js
@@ -17,8 +17,10 @@ class History extends Component {
             <h2>Questions asked by you</h2>
             <h3>Would you rather...</h3>
             {askedQuestions.map(question => {
-              return <div>
+
+              return <div key={question.id + 'rhej'}>
                 <p>...{question.optionOne.text} or {question.optionTwo.text}?</p>
+                <hr />
               </div>
             })}
           </div>
@@ -27,18 +29,30 @@ class History extends Component {
             <h2>Questions answered by you</h2>
             <h3>You would rather...</h3>
             {answeredQuestions.map(question => {
+
+              const votesOptionOne = question[2]
+              const votesOptionTwo = question[3]
+              const percentOne = (100 / (votesOptionOne + votesOptionTwo) * votesOptionOne)
+              const percentTwo = (100 / (votesOptionOne + votesOptionTwo) * votesOptionTwo)
+
               if (question[1] === 'optionOne') {
                 return (
                   <div key={question.id}>
                     <p>...<b>{question[0].optionOne.text}</b> than {question[0].optionTwo.text}!</p>
+                    <p>(Just like <b>{percentOne}%</b> of players.)</p>
+                    <hr />
                   </div>)
               } else {
                 return (
                   <div key={question.id}>
                     <p>...<b>{question[0].optionTwo.text}</b> than {question[0].optionOne.text}!</p>
+                    <p>(Just like <b>{percentTwo}%</b> of players.)</p>
+                    <hr />
                   </div>)
               }
+
             })}
+
           </div>
         </div>
       </div>
@@ -51,6 +65,7 @@ function mapStateToProps({ questions, authedUser, users }) {
   const askedQuestions = [];
   const answeredQuestions = [];
 
+
   users[authedUser].questions.forEach((question) => {
     askedQuestions.push(questions[question])
   })
@@ -58,7 +73,9 @@ function mapStateToProps({ questions, authedUser, users }) {
   for (let i in users[authedUser].answers) {
     answeredQuestions.push([
       questions[i], // the actual question object from questions | OBJECT
-      users[authedUser].answers[i] // the selected option for this question | STRING
+      users[authedUser].answers[i], // the selected option for this question | STRING
+      questions[i].optionOne.votes.length, // how many people voted for this option
+      questions[i].optionTwo.votes.length, // how many people voted for this option
     ]);
   }
 
