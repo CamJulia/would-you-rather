c7a9bcf5feef5e441f5b9dd5bc5a2b851b92cba0
Julia Çam <barbarbetsy@googlemail.com>
2019-07-28 17:24:33 +0200

    fix: show answered and unanswerd tab on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,37 +1,106 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 import Nav from './Nav';
 import Answer from './Answer';
 import History from './History';
 
+const answeredPollQuestion = (question, users) => {
+  const { question: q } = question;
+  return (
+    <div key={q.id}>
+      <h3>{users[q.author].name} asks:</h3>
+      <div>
+        <div>
+          <img src={users[q.author].avatarURL} alt="avatar_image" />
+        </div>
+        <div>
+          <h3>Would you rather</h3>
+          <p>{q.optionOne.text}</p>
+          <Link to={`/questions/${q.id}`}>
+            <button>View Poll</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 class Dashboard extends Component {
   state = {
-    view: 'answer'
+    view: 'unanswered'
   };
+
   render() {
+    const {
+      answeredQuestions,
+      unansweredQuestions,
+      authedUser,
+      users
+    } = this.props;
+
     return (
       <div>
         <Nav />
-        <div>
-          <button
-            onClick={() => {
-              this.setState({ view: 'answer' });
-            }}>
-            Answer
-          </button>
-          <button
-            onClick={() => {
-              this.setState({ view: 'history' });
-            }}>
-            History
-          </button>
+        <div className="dashboard">
+          <div className="dashboard-btns">
+            <button
+              onClick={() => {
+                this.setState({ view: 'unanswered' });
+              }}>
+              Unanswered
+            </button>
+            <button
+              onClick={() => {
+                this.setState({ view: 'answered' });
+              }}>
+              Answered
+            </button>
+          </div>
+          <div>
+            {this.state.view === 'unanswered'
+              ? unansweredQuestions.map(question =>
+                  answeredPollQuestion(question, users)
+                )
+              : answeredQuestions.map(question =>
+                  answeredPollQuestion(question, users)
+                )}
+          </div>
         </div>
-        <div>{this.state.view === 'answer' ? <Answer /> : <History />}</div>
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = ({ questions, authedUser, users }) => {
+  const answeredQuestions = [];
+  const unansweredQuestions = [];
+
+  for (let i in users[authedUser].answers) {
+    answeredQuestions.unshift({
+      question: questions[i], // the actual question object from questions | OBJECT
+      answer: users[authedUser].answers[i], // the selected option for this question | STRING
+      votesOptionOne: questions[i].optionOne.votes.length, // how many people voted for this option
+      votesOptionTwo: questions[i].optionTwo.votes.length // how many people voted for this option
+    });
+  }
+
+  const leAnsweredQuestions = Object.keys(users[authedUser].answers);
+
+  for (let questionKey in questions) {
+    if (leAnsweredQuestions.indexOf(questionKey) === -1) {
+      unansweredQuestions.unshift({ question: questions[questionKey] });
+    }
+  }
+
+  return {
+    answeredQuestions,
+    unansweredQuestions,
+    authedUser,
+    users
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
