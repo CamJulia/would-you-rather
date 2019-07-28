c94ec086448a6b69e417fd271dd6d99322a6b0bb
Julia Çam <barbarbetsy@googlemail.com>
2019-07-28 17:23:01 +0200

    feat: redirect to home on submit

--- src/components/Ask.js
+++ src/components/Ask.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import '../App.css';
-import { connect } from 'react-redux'
-import Nav from './Nav'
-import { saveQuestion } from '../actions/questions'
+import { connect } from 'react-redux';
+import Nav from './Nav';
+import { saveQuestion } from '../actions/questions';
 
 class Ask extends Component {
   state = {
     error: false,
     optionOne: '',
-    optionTwo: ''
-  }
+    optionTwo: '',
+    toHome: false
+  };
 
-  handleOptionInput = (e) => {
+  handleOptionInput = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
     if (this.state.error && value) {
       this.setState({ error: false });
     }
-  }
+  };
 
-  submitAnswer = (e) => {
+  submitAnswer = e => {
     e.preventDefault();
 
     const { optionOne, optionTwo } = this.state;
@@ -29,37 +31,65 @@ class Ask extends Component {
       return;
     }
 
-    this.props.dispatch(saveQuestion({ 
-      optionOneText: this.state.optionOne.trim(), 
-      optionTwoText: this.state.optionTwo.trim()
-    }));
+    this.props.dispatch(
+      saveQuestion({
+        optionOneText: this.state.optionOne.trim(),
+        optionTwoText: this.state.optionTwo.trim()
+      })
+    );
 
     this.setState({
       optionOne: '',
-      optionTwo: ''
+      optionTwo: '',
+      toHome: true
     });
-  }
+  };
 
   render() {
+    const { toHome } = this.state;
+
+    if (toHome) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div>
         <Nav />
-        <div className='ask'>
+        <div className="ask">
           <h1>Ask a question!</h1>
           <h2>Would you rather...</h2>
           <form>
-            <div className='form'>
-              <input type="text" name="optionOne" placeholder='Option one' value={this.state.optionOne} onChange={this.handleOptionInput} ></input>
+            <div className="form">
+              <input
+                type="text"
+                name="optionOne"
+                placeholder="Option one"
+                value={this.state.optionOne}
+                onChange={this.handleOptionInput}
+              />
               <p>or</p>
-              <input type="text" name="optionTwo" placeholder='Option two' value={this.state.optionTwo} onChange={this.handleOptionInput}></input>
+              <input
+                type="text"
+                name="optionTwo"
+                placeholder="Option two"
+                value={this.state.optionTwo}
+                onChange={this.handleOptionInput}
+              />
             </div>
-            <input className='submit' type="submit" value="Submit" onClick={this.submitAnswer} />
-            {this.state.error && <h3>Error! Each option must be at least 2 characters long.</h3>}
+            <input
+              className="submit"
+              type="submit"
+              value="Submit"
+              onClick={this.submitAnswer}
+            />
+            {this.state.error && (
+              <h3>Error! Each option must be at least 2 characters long.</h3>
+            )}
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect()(Ask)
+export default connect()(Ask);
