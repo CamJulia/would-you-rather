d61b8c44aebcb01589a520042f6ab95084dea403
Julia Çam <barbarbetsy@googlemail.com>
2019-07-30 15:28:59 +0200

    feat: add avatar to highscore

--- src/components/Highscore.js
+++ src/components/Highscore.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import '../App.css'
-import { connect } from 'react-redux'
-import Nav from './Nav'
+import React, { Component } from 'react';
+import '../App.css';
+import { connect } from 'react-redux';
+import Nav from './Nav';
 
 class Highscore extends Component {
   render() {
-
-    const { authedUser, userScores, users } = this.props
+    const { authedUser, userScores, users } = this.props;
 
     return (
       <div>
@@ -22,43 +21,54 @@ class Highscore extends Component {
                 <th>Score</th>
               </tr>
               {userScores.map((user, idx) => {
-                const { name, answers, questions, score } = user;
-                const me = users[authedUser].name
+                const { name, answers, questions, score, avatar } = user;
+                const me = users[authedUser].name;
 
                 if (name === me) {
-                  return <tr className='myScore' key={idx}>
-                    <td>{name}</td>
-                    <td>{answers}</td>
-                    <td>{questions}</td>
-                    <td>{score}</td>
-                  </tr>
+                  return (
+                    <tr className="myScore" key={idx}>
+                      <td>
+                        <img src={avatar} alt="avatar_image" />
+                        {name}
+                      </td>
+                      <td>{answers}</td>
+                      <td>{questions}</td>
+                      <td>{score}</td>
+                    </tr>
+                  );
                 } else {
-                  return <tr key={idx}>
-                    <td>{name}</td>
-                    <td>{answers}</td>
-                    <td>{questions}</td>
-                    <td>{score}</td>
-                  </tr>
+                  return (
+                    <tr key={idx}>
+                      <td>
+                        <img src={avatar} alt="avatar_image" />
+                        {name}
+                      </td>
+                      <td>{answers}</td>
+                      <td>{questions}</td>
+                      <td>{score}</td>
+                    </tr>
+                  );
                 }
               })}
             </tbody>
           </table>
         </div>
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps({ authedUser, users }) {
-
-  let userScores = []
+  let userScores = [];
 
   for (let i in users) {
     userScores.push({
       name: users[i]['name'], // gets users name
       answers: Object.keys(users[i]['answers']).length, // gets number of answers
       questions: users[i]['questions'].length, // gets number of questions
-      score: (Object.keys(users[i]['answers']).length) + (users[i]['questions'].length) //get score 
+      score:
+        Object.keys(users[i]['answers']).length + users[i]['questions'].length, //get score
+      avatar: users[i]['avatarURL']
     });
   }
 
@@ -69,8 +79,8 @@ function mapStateToProps({ authedUser, users }) {
   return {
     authedUser,
     users,
-    userScores,
-  }
+    userScores
+  };
 }
 
-export default connect(mapStateToProps)(Highscore)
+export default connect(mapStateToProps)(Highscore);
