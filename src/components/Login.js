2175c30341a6a68f1141613c9bed59c43b535944
Julia Çam <barbarbetsy@googlemail.com>
2019-08-04 14:08:52 +0200

    add redirect to dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,42 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { setAuthedUser } from '../actions/authedUser'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
 
 class Login extends Component {
-  setAuthUser = (e) => {
+  setAuthUser = e => {
     const name = e.currentTarget.getAttribute('name');
-    this.props.dispatch(setAuthedUser(name))
-  }
+    this.props.dispatch(setAuthedUser(name));
+  };
 
   render() {
+    const { usersIds, users, authedUser } = this.props;
 
-    const { usersIds, users } = this.props;
     if (!usersIds.length) return null;
-
+    if (authedUser) return <Redirect to="/" />;
 
     return (
       <div>
         <h1>Would you rather be...</h1>
-        <div className='login'>
-          {usersIds.map((player) => {
-
-            return <button className='player' key={users[player].id} name={users[player].id} onClick={this.setAuthUser}>
-              <img src={users[player].avatarURL} alt={`Avatar of ${users[player].name}`} />
-              <p>{users[player].name}</p>
-            </button >
+        <div className="login">
+          {usersIds.map(player => {
+            return (
+              <button
+                className="player"
+                key={users[player].id}
+                name={users[player].id}
+                onClick={this.setAuthUser}>
+                <img
+                  src={users[player].avatarURL}
+                  alt={`Avatar of ${users[player].name}`}
+                />
+                <p>{users[player].name}</p>
+              </button>
+            );
           })}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -37,7 +46,7 @@ function mapStateToProps({ questions, authedUser, users }) {
     authedUser,
     users,
     questions
-  }
+  };
 }
 
-export default connect(mapStateToProps)(Login)
+export default connect(mapStateToProps)(Login);
