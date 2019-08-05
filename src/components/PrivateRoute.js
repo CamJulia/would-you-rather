f21156f00c3a6cb2cec2c67d5a4065eab310bc08
Julia Çam <barbarbetsy@googlemail.com>
2019-08-05 21:20:58 +0200

    spread all props together

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, authedUser }) => {
+const PrivateRoute = ({ component: Component, ...passedProps }) => {
   return (
     <Route
       render={props => {
-        return authedUser ? (
-          <Component {...props} />
+        return passedProps.authedUser ? (
+          <Component {...{ ...props, ...passedProps }} />
         ) : (
           <Redirect
             to={{
