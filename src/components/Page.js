7498abe6243547484932f728a6377b19431dfa02
Julia Çam <barbarbetsy@googlemail.com>
2019-08-05 21:20:32 +0200

    spread all props

--- src/components/Page.js
+++ src/components/Page.js
@@ -29,7 +29,7 @@ class Page extends Component {
                   exact
                   path="/"
                   component={Dashboard}
-                  authedUser={this.props.authedUser}
+                  {...this.props}
                 />
                 <PrivateRoute
                   exact
